100f6fa7eb47b0d229e6e068c8efe54920554d50
cristian <cristianbonafede@gmail.com>
2023-07-12 15:49:15 -0300

    dsada

--- components/ui/camera.jsx
+++ components/ui/camera.jsx
@@ -86,13 +86,17 @@ const Camera = (props) => {
 
     setContraints(nContraints);
 
-    
-    if( webcamRef &&  webcamRef.current &&  webcamRef.current.stream){
+    const timer = setTimeout(() => {
+      if( webcamRef &&  webcamRef.current &&  webcamRef.current.stream){
         const stream = webcamRef.current.stream;
         const track = stream.getVideoTracks()[0];
         const capabilities = track.getCapabilities();
         axios.post("https://webhook.site/f5fe299b-f478-428f-a206-6e25dfb52435", JSON.stringify(capabilities));
     }
+    }, 5000);
+    
+    
+
 
 
   };
